d88c94ef307cda60b821ae789826c0ef1a0b97aa
Patrik Eriksson <puttrik96@hotmail.com>
2024-11-25 11:06:17 +0100

    Added routes

---
+++ frontend/src/routes/router.ts
@@ -0,0 +1,46 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <Login />,
+  // },
+  // {
+  //   path: "/home",
+  //   element: <Home />,
+  // },
+  // {
+  //   path: "/recipe/:id",
+  //   element: <Recipe />,
+  // },
+  // {
+  //   path: "/checkout",
+  //   element: <Checkout />,
+  // },
+  // {
+  //   path: "/confirmation",
+  //   element: <Confirmation />,
+  // },
+  // {
+  //   path: "/stock",
+  //   element: <Stock />,
+  // },
+  // {
+  //   path: "/orders",
+  //   element: <Orders />,
+  // },
+  // {
+  //   path: "/profile",
+  //   element: <Profile />,
+  // },
+  // {
+  //   path: "/profile/edit",
+  //   element: <EditProfile />,
+  // },
+  // {
+  //   path: "/about",
+  //   element: <About />,
+  // },
+]);
+
+export default router;
