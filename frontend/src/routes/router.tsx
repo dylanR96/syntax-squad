e842cf76ea0862db84eb2445a395721c073151b3
Patrik Eriksson <puttrik96@hotmail.com>
2024-11-25 13:55:19 +0100

    Fixed routes, started on Login page

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <Login />,
-  // },
+  {
+    path: "/",
+    element: <Login />,
+  },
   // {
   //   path: "/home",
   //   element: <Home />,
