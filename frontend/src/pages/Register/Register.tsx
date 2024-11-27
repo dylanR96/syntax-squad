2d21afd93e09d7bb19650fe91563313f0d442794
Patrik Eriksson <puttrik96@hotmail.com>
2024-11-27 17:55:35 +0100

    Login and register desktop layout. Changed svg tags to img.

--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,46 +1,58 @@
 import logo from "../../assets/images/easybakelogo.svg";
-import { NavLink } from "react-router-dom";
+import dottedLine from "../../assets/images/dotted-line.svg";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <main className="login">
-      <img src={logo} alt="Easybake logo" />
-      <h1 className="login__title">Easybake</h1>
-      <form className="login-form" action="">
-        <input
-          className="login-form__input"
-          type="text"
-          placeholder="Förnamn"
-        />
-        <input
-          className="login-form__input"
-          type="text"
-          placeholder="Efternamn"
-        />
-        <input className="login-form__input" type="email" placeholder="Email" />
-        <input
-          className="login-form__input"
-          type="password"
-          placeholder="Lösenord"
-        />
-        <input
-          className="login-form__input"
-          type="password"
-          placeholder="Bekräfta lösenord"
-        />
-        <button className="login-form__button" type="submit">
-          Registrera
-        </button>
-      </form>
+    <main className="main-login">
+      <section className="login">
+        <img src={logo} alt="Easybake logo" />
+        <h1 className="login__title">Easybake</h1>
+        <form className="login-form" action="">
+          <input
+            className="login-form__input"
+            type="text"
+            placeholder="Förnamn"
+          />
+          <input
+            className="login-form__input"
+            type="text"
+            placeholder="Efternamn"
+          />
+          <input
+            className="login-form__input"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            className="login-form__input"
+            type="password"
+            placeholder="Lösenord"
+          />
+          <input
+            className="login-form__input"
+            type="password"
+            placeholder="Bekräfta lösenord"
+          />
+          <button className="login-form__button" type="submit">
+            Registrera
+          </button>
+        </form>
 
-      <div className="alternative-login">
-        <p className="alternative-login__text">
-          Har du redan ett konto?{" "}
-          <NavLink to={"/"} className="alternative-login__link">
-            Logga in
-          </NavLink>
-        </p>
-      </div>
+        <div className="alternative-login">
+          <p className="alternative-login__text">
+            Har du redan ett konto?{" "}
+            <Link to={"/"} className="alternative-login__link">
+              Logga in
+            </Link>
+          </p>
+        </div>
+      </section>
+
+      <div className="slogan-background"></div>
+      <h2 className="slogan-top">Vi står för måtten</h2>
+      <h2 className="slogan-bottom">Du står för magin</h2>
+      <img className="dotted-line" src={dottedLine} />
     </main>
   );
 };
