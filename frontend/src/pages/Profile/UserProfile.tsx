778f72712a7cbbbe665f7efed67151a3fd33a3d0
Erik Karlsson <erikmatfors@gmail.com>
2024-11-27 13:16:46 +0100

    OrderEntry component

--- frontend/src/pages/Profile/UserProfile.tsx
+++ frontend/src/pages/Profile/UserProfile.tsx
@@ -3,6 +3,7 @@ import "./UserProfile.css";
 import profilePicture from "../../assets/images/profile_picture.png";
 import Header from "../../components/layout/Header/Header";
 import Footer from "../../components/layout/Footer/Footer";
+import OrderEntry from "../../components/forms/OrderEntry";
 
 const Profile: React.FC = () => {
   return (
@@ -14,21 +15,9 @@ const Profile: React.FC = () => {
     <h2 className="profile__name">John Dough</h2>
     </div>
     <h2 className="profile__header">Orderhistorik</h2>
-    <div className="profile__order-history">
-    <a href="#"><p className="profile__order-number">874763472</p></a>
-      <p className="profile__order-amount">80 kr</p>
-      <p className="profile__order-date">2021-09-01 11:43</p>
-    </div>
-    <div className="profile__order-history">
-    <a href="#"><p className="profile__order-number">874763472</p></a>
-      <p className="profile__order-amount">180 kr</p>
-      <p className="profile__order-date">2021-10-15 12:54</p>
-    </div>
-    <div className="profile__order-history">
-    <a href="#"><p className="profile__order-number">874763472</p></a>
-      <p className="profile__order-amount">59 kr</p>
-      <p className="profile__order-date">2023-04-05 08:15</p>
-    </div>
+    <OrderEntry />
+    <OrderEntry />
+    <OrderEntry />
     <button className="profile__edit-button" type="submit">Ändra profil</button>
     <button className="profile__logout-button" type="submit">Logga ut</button>
   </main>
