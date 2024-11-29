46e3ac9eb12fe3495fcbe4bd25986c23370aedb8
Andreas Bergström <andreas@textalkgroup.com>
2024-11-29 09:03:51 +0100

    started a modal

--- frontend/src/pages/Stock/Stock.tsx
+++ frontend/src/pages/Stock/Stock.tsx
@@ -31,38 +31,43 @@ const Stock = () => {
   }, []);
 
   return (
-    <main className="stock-container">
-      <h1 className="h1--dark stock-heading">Lagersaldo</h1>
-      <article>
-        <div className="stock-wrapper stock-wrapper--title">
-          <div className="stock__buttons"></div>
-          <div className="stock__ingredient">Ingrediens</div>
-          <div className="stock__number">Antal</div>
-          <div className="stock__unit">Enhet</div>
-        </div>
-        {ingredients &&
-          ingredients.map((ingredient) => {
-            // ingredient.ingredientID
-            return (
-              <>
-                <div className="stock-wrapper">
-                  <div className="stock__buttons">
-                    <button className="stock__button">
-                      <img src={editIcon} className="stock__button--icon" />
-                    </button>
-                  </div>
+    <>
+      <section className="stock-modal">
+        <h1 className="h1--dark">Ändra</h1>
+      </section>
+      <main className="stock-container">
+        <h1 className="h1--dark stock-heading">Lagersaldo</h1>
+        <article>
+          <div className="stock-wrapper stock-wrapper--title">
+            <div className="stock__buttons"></div>
+            <div className="stock__ingredient">Ingrediens</div>
+            <div className="stock__number">Antal</div>
+            <div className="stock__unit">Enhet</div>
+          </div>
+          {ingredients &&
+            ingredients.map((ingredient) => {
+              // ingredient.ingredientID
+              return (
+                <>
+                  <div className="stock-wrapper">
+                    <div className="stock__buttons">
+                      <button className="stock__button">
+                        <img src={editIcon} className="stock__button--icon" />
+                      </button>
+                    </div>
 
-                  <div className="stock__ingredient">
-                    {ingredient.ingredientName}
+                    <div className="stock__ingredient">
+                      {ingredient.ingredientName}
+                    </div>
+                    <div className="stock__number">{ingredient.stock}</div>
+                    <div className="stock__unit">{ingredient.units}</div>
                   </div>
-                  <div className="stock__number">{ingredient.stock}</div>
-                  <div className="stock__unit">{ingredient.units}</div>
-                </div>
-              </>
-            );
-          })}
-      </article>
-    </main>
+                </>
+              );
+            })}
+        </article>
+      </main>
+    </>
   );
 };
 
