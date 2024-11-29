7d2cbb62bc9efcae1bddf0b1498ded8961f709e0
Erik Karlsson <erikmatfors@gmail.com>
2024-11-29 13:13:41 +0100

    Testimonials

--- frontend/src/pages/About/About.tsx
+++ frontend/src/pages/About/About.tsx
@@ -13,6 +13,17 @@ const AboutUs = () => {
     <article className="about__textbox">
       <h1 className="about__header">Om oss</h1>
       <p className="about__infotext">
+        Vi på EasyBake tror att bakning är en konstform som alla kan njuta av. Därför har vi skapat en plattform där du kan hitta inspiration, recept och ingredienser för att skapa ditt eget mästerverk.
+        </p>
+        <br />
+        <p className="about__infotext">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+        scelerisque, risus et consectetur laoreet, lacus libero fermentum
+        ligula, vitae ultricies nunc nisl nec nunc. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Ea, porro culpa! Corrupti earum quo voluptas ducimus excepturi consequuntur nihil fugiat.
+        </p>
+        <br />
+        <p className="about__infotext">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
         scelerisque, risus et consectetur laoreet, lacus libero fermentum
         ligula, vitae ultricies nunc nisl nec nunc. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
@@ -20,6 +31,8 @@ const AboutUs = () => {
         </p>
     </article>
     </section>
+    <h1 className='h1--dark'>"Bake the world a better place"</h1>
+    <h4 className='h4--dark'>Unknown</h4>
     <TestimonialCarousel />
     <Gallery />
     </main>
