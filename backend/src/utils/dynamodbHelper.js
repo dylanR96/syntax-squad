fa25273360c7479d481c19dec775d2b56250bed3
Andreas Bergström <skrap@andreasb.se>
2024-12-11 20:48:35 +0100

    added changeStock function, and reroute admin without access

--- backend/src/utils/dynamodbHelper.js
+++ backend/src/utils/dynamodbHelper.js
@@ -3,6 +3,5 @@ export const createID = async (TABLE_NAME, ID_KEY, START_NUMBER) => {
   const { Items } = await db.scan({ TableName: TABLE_NAME });
   const ids = Items.map((item) => item[ID_KEY]);
   const maxId = ids.length > 0 ? Math.max(...ids) : START_NUMBER;
-  console.log(maxId + 1);
   return maxId + 1;
 };
