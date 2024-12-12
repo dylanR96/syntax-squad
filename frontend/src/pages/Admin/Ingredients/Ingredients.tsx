8e58d731ce753b55028b44654534d07855eeb3f9
Andreas Bergström <andreas@textalkgroup.com>
2024-12-12 07:39:02 +0100

    added loader

--- frontend/src/pages/Admin/Ingredients/Ingredients.tsx
+++ frontend/src/pages/Admin/Ingredients/Ingredients.tsx
@@ -4,6 +4,7 @@ import { EditIngredientType, IngredientType } from "./types";
 import StockModal from "./IngredientModal";
 import NewIngredient from "./NewIngredient";
 import { ENDPOINT_ALL_INGREDIENTS } from "../../../endpoints/apiEndpoints";
+import Loader from "../../../components/ui/Loader";
 
 const Stock = () => {
   const [ingredients, setIngredients] = useState<IngredientType[]>([]);
@@ -64,81 +65,89 @@ const Stock = () => {
         />
       )}
       <main className="stock-container">
-        <h1 className="h1--dark stock-heading">Lagersaldo</h1>
-        <div className="stock-wrapper">
-          <section>
-            {(lowStock.length > 0 || outOfStock.length > 0) && (
-              <h4 className="h4--dark stock--h4">Låga saldon</h4>
-            )}
+        {ingredients.length > 0 ? (
+          <>
+            <h1 className="h1--dark stock-heading">Lagersaldo</h1>
+            <div className="stock-wrapper">
+              <section>
+                {(lowStock.length > 0 || outOfStock.length > 0) && (
+                  <h4 className="h4--dark stock--h4">Låga saldon</h4>
+                )}
 
-            <article className="stock-alert">
-              <div className="stock-alert stock-alert--zero">
-                {outOfStock.length > 0 &&
-                  outOfStock.map((ingredient) => {
-                    return (
-                      <div
-                        className="alert__ingredient alert__ingredient--zero"
-                        key={`${ingredient.ingredientName}-oos`}
-                        onClick={() => {
-                          initEditIngredient(ingredient);
-                        }}
-                      >
-                        {ingredient.ingredientName} - {ingredient.stock}{" "}
-                        {ingredient.units}
-                      </div>
-                    );
-                  })}
-              </div>
-              <div className="stock-alert stock-alert--low">
-                {lowStock.length > 0 &&
-                  lowStock.map((ingredient) => {
-                    return (
-                      <div
-                        className="alert__ingredient alert__ingredient--low"
-                        key={`${ingredient.ingredientName}-low`}
-                        onClick={() => {
-                          initEditIngredient(ingredient);
-                        }}
-                      >
-                        {ingredient.ingredientName} - {ingredient.stock}{" "}
-                        {ingredient.units}
-                      </div>
-                    );
-                  })}
-              </div>
-            </article>
-          </section>
-          <section className="ingredients-list__wrapper">
-            <h4 className="h4--dark stock--h4">Ingredienslista</h4>
-            <article className="stock__ingredients-list">
-              <div className="ingredient-wrapper ingredient-wrapper--title">
-                <div className="stock__ingredient">Ingrediens</div>
-                <div className="stock__number">Antal</div>
-                <div className="stock__unit">Enhet</div>
-              </div>
-              {ingredients &&
-                ingredients.map((ingredient) => {
-                  // ingredient.ingredientID
-                  return (
-                    <div
-                      className="ingredient-wrapper"
-                      key={ingredient.ingredientID}
-                      onClick={() => {
-                        initEditIngredient(ingredient);
-                      }}
-                    >
-                      <div className="stock__ingredient">
-                        {ingredient.ingredientName}
-                      </div>
-                      <div className="stock__number">{ingredient.stock}</div>
-                      <div className="stock__unit">{ingredient.units}</div>{" "}
-                    </div>
-                  );
-                })}
-            </article>
-          </section>
-        </div>
-        <NewIngredient units={units} setRerender={setRerender} />
+                <article className="stock-alert">
+                  <div className="stock-alert stock-alert--zero">
+                    {outOfStock.length > 0 &&
+                      outOfStock.map((ingredient) => {
+                        return (
+                          <div
+                            className="alert__ingredient alert__ingredient--zero"
+                            key={`${ingredient.ingredientName}-oos`}
+                            onClick={() => {
+                              initEditIngredient(ingredient);
+                            }}
+                          >
+                            {ingredient.ingredientName} - {ingredient.stock}{" "}
+                            {ingredient.units}
+                          </div>
+                        );
+                      })}
+                  </div>
+                  <div className="stock-alert stock-alert--low">
+                    {lowStock.length > 0 &&
+                      lowStock.map((ingredient) => {
+                        return (
+                          <div
+                            className="alert__ingredient alert__ingredient--low"
+                            key={`${ingredient.ingredientName}-low`}
+                            onClick={() => {
+                              initEditIngredient(ingredient);
+                            }}
+                          >
+                            {ingredient.ingredientName} - {ingredient.stock}{" "}
+                            {ingredient.units}
+                          </div>
+                        );
+                      })}
+                  </div>
+                </article>
+              </section>
+              <section className="ingredients-list__wrapper">
+                <h4 className="h4--dark stock--h4">Ingredienslista</h4>
+                <article className="stock__ingredients-list">
+                  <div className="ingredient-wrapper ingredient-wrapper--title">
+                    <div className="stock__ingredient">Ingrediens</div>
+                    <div className="stock__number">Antal</div>
+                    <div className="stock__unit">Enhet</div>
+                  </div>
+                  {ingredients &&
+                    ingredients.map((ingredient) => {
+                      // ingredient.ingredientID
+                      return (
+                        <div
+                          className="ingredient-wrapper"
+                          key={ingredient.ingredientID}
+                          onClick={() => {
+                            initEditIngredient(ingredient);
+                          }}
+                        >
+                          <div className="stock__ingredient">
+                            {ingredient.ingredientName}
+                          </div>
+                          <div className="stock__number">
+                            {ingredient.stock}
+                          </div>
+                          <div className="stock__unit">{ingredient.units}</div>{" "}
+                        </div>
+                      );
+                    })}
+                </article>
+              </section>
+            </div>
+            <NewIngredient units={units} setRerender={setRerender} />
+          </>
+        ) : (
+          <Loader />
+        )}
       </main>
     </>
   );
